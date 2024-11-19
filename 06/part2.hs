2c17d1e8a522eb593fdb400b702f886dea9c232f
Lukas Boehm <lukas.l.boehm@fau.de>
2024-11-19 16:52:48 +0100

    06 done

--- 06/part2.hs
+++ 06/part2.hs
@@ -3,17 +3,17 @@ raceDist :: Int -> Int -> Int
 raceDist totalTime accTime = (totalTime - accTime) * accTime
 
 waysToBeatRecord :: (Int, Int) -> Int
-waysToBeatRecord (time, record) = foldl go 0 [1 .. time]
+waysToBeatRecord (time, record) = foldl go 0 [0 + 14 .. time - 14]
   where
     go :: Int -> Int -> Int
     go acc x = if raceDist time x > record then acc + 1 else acc
 
 main = do
   txt <- readFile "input.txt"
-  let times :: [Int] = map (read :: String -> Int) $ words $ drop 10 (lines txt !! 0)
-  let records :: [Int] = map (read :: String -> Int) $ words $ drop 10 (lines txt !! 1)
+  let noSpaces = filter (/= ' ') txt
+  let time :: Int = read $ drop 5 (lines noSpaces !! 0)
+  let record :: Int = read $ drop 9 (lines noSpaces !! 1)
 
-  let ways = map waysToBeatRecord $ zip times records
+  let ways = waysToBeatRecord (time, record)
 
   print $ ways
-  print $ product ways
