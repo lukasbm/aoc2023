749b1e26fc68abb2a1b0e101166e35a6c0811671
Lukas Boehm <lukas.l.boehm@fau.de>
2024-11-23 01:10:33 +0100

    07 part 1 lamost done

--- 07/part1.hs
+++ 07/part1.hs
@@ -1,24 +1,13 @@
-import Data.List (sort)
+import Data.List (sort, sortBy)
+import Data.Ord (comparing)
 
 --------- data structures
 
-data Combo = NoCombo | HighCard | OnePair | TwoPair | ThreeOfAKind | FullHouse | FourOfAKind | FiveOfAKind deriving (Show, Eq, Ord)
+data Combo = NoCombo | HighCard | OnePair | TwoPair | ThreeOfAKind | FullHouse | FourOfAKind | FiveOfAKind deriving (Show, Eq, Ord, Enum)
 
-data Card = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving (Show, Eq, Ord)
+data Card = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving (Show, Eq, Ord, Enum)
 
-newtype Hand = Hand (Card, Card, Card, Card, Card) deriving (Show)
-
--- order based on the card value
-instance Ord Hand where
-  compare :: Hand -> Hand -> Ordering
-  compare (Hand (a1, a2, a3, a4, a5)) (Hand (b1, b2, b3, b4, b5)) = do
-    let scoreHand1 = 10000 * fromEnum a1 + 1000 * fromEnum a2 + 100 * fromEnum a3 + 10 * fromEnum a4 + fromEnum a5
-    let scoreHand2 = 10000 * fromEnum b1 + 1000 * fromEnum b2 + 100 * fromEnum b3 + 10 * fromEnum b4 + fromEnum b5
-    compare scoreHand1 scoreHand2
-
-instance Eq Hand where
-  (==) :: Hand -> Hand -> Bool
-  (Hand (a1, a2, a3, a4, a5)) == (Hand (b1, b2, b3, b4, b5)) = a1 == b1 && a2 == b2 && a3 == b3 && a4 == b4 && a5 == b5
+type Hand = (Card, Card, Card, Card, Card)
 
 --------- parsing
 
@@ -39,7 +28,7 @@ parseCard 'A' = Ace
 parseCard _ = error "Invalid card"
 
 parseHand :: [Char] -> Hand
-parseHand (a : b : c : d : e : _) = Hand (parseCard a, parseCard b, parseCard c, parseCard d, parseCard e)
+parseHand (a : b : c : d : e : _) = (parseCard a, parseCard b, parseCard c, parseCard d, parseCard e)
 
 main :: IO ()
 main = do
@@ -48,36 +37,49 @@ main = do
   let hands :: [Hand] = map (parseHand . take 5) (lines txt)
   let bids :: [Int] = map ((read :: String -> Int) . drop 6) (lines txt)
 
-  print "hi"
+  print $ fromEnum TwoPair
+  print $ fromEnum Three
 
-  let h1 = Hand (Ace, King, Queen, Jack, Ten)
-  let h2 = Hand (Ace, King, Queen, Jack, Nine)
-  let h3 = Hand (Two, Four, Three, Three, Three)
+  -- solution
+  print $ bids
+  print $ rankHands hands
 
-  -- print $ h1 == h2
-  -- print $ h1 < h2
+  -- FIXME: need to sort bids together with the hand ranks!!!!
 
-  print $ sort h3
+  let winnings = zipWith (+) (rankHands hands) bids
+  print $ sum winnings
 
 --------- solution
 
-determineCombo :: Hand -> Combo
-determineCombo
-  | isFiveOfAKind = FiveOfAKind
-  | isFourOfAKind = FourOfAKind
-  | isFullHouse = FullHouse
-  | isThreeOfAKind = ThreeOfAKind
-  | isTwoPair = TwoPair
-  | isOnePair = OnePair
-  | isHighCard = HighCard
-  | otherwise = HighCard
-  where
-    isFiveOfAKind = a == b && b == c && c == d && d == e
-    isFourOfAKind = (a == b && b == c && c == d) || (b == c && c == d && d == e)
-    isFullHouse = (a == b && b == c && d == e) || (a == b && c == d && d == e)
+-- Helper Function to get sorting indices
+-- NOTE: 1 based index
+sortingIndices :: (Ord a) => [a] -> [Int]
+sortingIndices xs = map snd $ sortBy (comparing fst) $ zip xs [1 ..]
 
+-- sort the hands based on the combo and the card values
+rankHands :: [Hand] -> [Int]
+rankHands hands = sortingIndices $ map handScore hands
+
+-- TODO: work here!
+determineCombo :: Hand -> Combo
+determineCombo (a, b, c, d, e) =
+  let sortedCards = sort [a, b, c, d, e]
+   in case sortedCards of
+        [s1, s2, s3, s4, s5] -> determineComboFromSorted (s1, s2, s3, s4, s5)
+
+determineComboFromSorted :: Hand -> Combo
+determineComboFromSorted (a, b, c, d, e)
+  | a == b && b == c && c == d && d == e = FiveOfAKind
+  | (a == b && b == c && c == d) || (b == c && c == d && d == e) = FourOfAKind
+  | (a == b && b == c && d == e) || (a == b && c == d && d == e) = FullHouse
+  | (a == b && b == c) && (d == e) || (a == b) && (c == d && d == e) = ThreeOfAKind
+  | (a == b && c == d) || (a == b && d == e) || (b == c && d == e) = TwoPair
+  | a == b || b == c || c == d || d == e = OnePair
+  | otherwise = HighCard
+
+-- a combo is always strong than any lower combo with higher card values
 -- 100 * combo + card
 -- NOTE: card value is not only determined by the first card, but by the whole hand
 -- need to do pairwise comparison, then sort the list
-calcScore :: Hand -> Int
-calcScore hands = 100 * fromEnum (determineCombo hands) + fromEnum (head hands)
+handScore :: Hand -> Int
+handScore hand@(a, b, c, d, e) = 100000 * fromEnum (determineCombo hand) + 10000 * fromEnum a + 1000 * fromEnum b + 100 * fromEnum c + 10 * fromEnum d + fromEnum e
